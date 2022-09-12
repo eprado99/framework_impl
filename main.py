a1037f7fa25e82a0a9a8ee191198513cb7a7a66f
eprado99 <a01570318@itesm.mx>
2022-09-11 19:04:09 -0500

    added linear regression using scikit learn and finished graphics on both impls

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import pandas as pd
 df = pd.read_csv('EstaturaPeso_HM.csv')
 # drop data corresponding to female columns to focus in only one
 df = df.drop(['M_estat', 'M_peso'], axis = 1)
-print(df)
 
 # Cost function
 def mse(y, pred, y_size):
@@ -27,7 +26,7 @@ def grad_descent(curr_slope, curr_intercept, data, alpha):
         
         x = data.iloc[i].H_estat
         y = data.iloc[i].H_peso
-        print(x, y)
+        # print(x, y)
         # Our y hat
         pred = curr_slope * x + curr_intercept
         # Use derivatives of mse
@@ -38,9 +37,38 @@ def grad_descent(curr_slope, curr_intercept, data, alpha):
     new_b = curr_intercept - calculated_b * alpha
     return new_m, new_b
 
+def abline(slope, intercept):
+    axes = plt.gca()
+    x_vals = np.array(axes.get_xlim())
+    y_vals = intercept + slope * x_vals
+    plt.plot(x_vals, y_vals, '--')
 
-m, b = grad_descent(0, 0, df, 0.01)
-print(m, b)
+m = 0
+b = 0
+for i in range(1000):
+    print(i)
+    m, b = grad_descent(m, b, df, 0.01)
 
 plt.scatter(df.H_estat, df.H_peso, color = 'blue')
+abline(m, b)
 plt.show()
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+X = np.array(df['H_estat']).reshape(-1, 1)
+y = np.array(df['H_peso']).reshape(-1, 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+
+regr = LinearRegression()
+
+regr.fit(X_train, y_train)
+
+y_pred = regr.predict(X_test)
+plt.scatter(X_test, y_test, color ='b')
+plt.plot(X_test, y_pred, color ='k')
+
+plt.show()
+
+
